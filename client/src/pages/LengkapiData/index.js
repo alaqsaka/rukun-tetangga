4ce8b8cdf56d51e0c5bb909660ce72213b130d84
alaqsaka <aqsaabidin15@gmail.com>
2022-05-30 21:03:44 +0700

    add ketua validation on page /

--- client/src/pages/LengkapiData/index.js
+++ client/src/pages/LengkapiData/index.js
@@ -1,11 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import {
+  Avatar,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+  TextField,
+  RadioGroup,
+  Radio,
+  FormLabel,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  FormControl
+} from '@mui/material';
+import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+import * as Yup from 'yup';
+import Input from './Input';
+import { Formik, Form } from 'formik';
+import Button from './Button';
 
 const LengkapiData = () => {
+  const classes = useStyles();
   const history = useHistory();
   const location = useLocation();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const dispatch = useDispatch();
 
   useEffect(() => {
     //const token = user?.token;
@@ -13,14 +36,138 @@ const LengkapiData = () => {
     setUser(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
 
+  const INITIAL_FORM_STATE_KETUA = {
+    community_address: '',
+    community_id: '',
+    community_nama: ''
+  };
+
+  const INITIAL_FORM_STATE_WARGA = {
+    community_id: ''
+  };
+
+  const [formData, setFormData] = useState(INITIAL_FORM_STATE_KETUA);
+
+  const FORM_VALIDATION_KETUA = Yup.object().shape({
+    community_address: Yup.string().required('Alamat RT-mu tidak boleh kosong'),
+    community_id: Yup.string().required('Auto generate ID'),
+    community_nama: Yup.string().required('Sebutan untuk RT-mu')
+  });
+
+  const FORM_VALIDATION_WARGA = Yup.object().shape({
+    community_id: Yup.string().required('Auto generate ID')
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (values) => {
+    console.log('submit');
+    console.log(formData);
+    console.log(values);
+    //dispatch(signup(values, history));
+  };
+
+  const handleSubmitWarga = (values) => {
+    //dispatch(signin(values, history));
+  };
+
+  const handleReset = () => {
+    // if (!window.confirm('Reset?')) {
+    //   throw new Error('Cancel reset');
+    // }
+  };
+
+  let initalFormValues = {};
+
+  if (user.result.role === 'ketua') {
+    initalFormValues = { ...INITIAL_FORM_STATE_KETUA };
+  } else {
+    initalFormValues = { ...INITIAL_FORM_STATE_WARGA };
+  }
+
   return (
-    <div>
-      {user.result.role == 'ketua' ? (
-        <div>lengkapi data untuk ketua</div>
-      ) : (
-        user.result.role == 'warga' && <div>lengkapi data untuk warga</div>
-      )}
-    </div>
+    <Container component="main" maxWidth="md">
+      <Paper className={classes.paper} elevation={3}>
+        <Typography variant="h5" style={{ textAlign: 'center' }}>
+          Lengkapi Datamu Terlebih Dahulu Yuk!
+        </Typography>
+        <Formik
+          initialValues={{
+            ...initalFormValues
+          }}
+          //   validationSchema={
+          //     user.result.role === 'ketua'
+          //       ? FORM_VALIDATION_KETUA
+          //       : FORM_VALIDATION_WARGA
+          //   }
+          validator={() => ({})}
+          onSubmit={(values) => {
+            console.log('submit');
+            handleSubmit(values);
+            // if (user.result.role === 'ketua') {
+            //   // signup
+            //   console.log(values);
+            //   // dispatch(signup(values, history));
+            //   handleSubmit(values);
+            // } else {
+            //   // signin
+            //   console.log(values);
+            //   // dispatch(signin(values, history));
+            //   handleSubmitWarga(values);
+            // }
+          }}
+          onReset={handleReset}
+        >
+          <Form>
+            <Grid container spacing={2}>
+              {user.result.role === 'ketua' ? (
+                <>
+                  <Input
+                    name="community_address"
+                    label="Alamat RT-mu"
+                    handleChange={handleChange}
+                  />
+                  <Input
+                    name="community_nama"
+                    label="Sebutan nama RT-mu"
+                    handleChange={handleChange}
+                  />
+
+                  <Input
+                    name="community_id"
+                    label="Id community"
+                    handleChange={handleChange}
+                  />
+                  <Grid item md={12}>
+                    <Typography variant="h6">
+                      Kode ini yang nantinya kamu bagikan kepada warga ya!
+                    </Typography>
+                  </Grid>
+                </>
+              ) : (
+                <>
+                  <Typography variant="h6">
+                    Masukkan kode yang diberikan oleh ketua RT-mu ya
+                  </Typography>
+                  <Input
+                    name="community_id"
+                    label="Id community"
+                    handleChange={handleChange}
+                  />
+                </>
+              )}
+              <Grid item xs={12}>
+                <Button variant="contained" fullWidth>
+                  Lengkapi Data
+                </Button>
+              </Grid>
+            </Grid>
+          </Form>
+        </Formik>
+      </Paper>
+    </Container>
   );
 };
 
