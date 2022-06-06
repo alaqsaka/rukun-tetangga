2b8586aa991b27ea81242eb9daaf2d54e7fa0a9c
alaqsaka <aqsaabidin15@gmail.com>
2022-06-07 01:47:50 +0700

    feat: edit welcome page

--- client/src/pages/Welcome/index.jsx
+++ client/src/pages/Welcome/index.jsx
@@ -1,25 +1,31 @@
 import React, { useState, useEffect } from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
 import { Hero, Section, ContactUs, AboutSection } from '../../components/atoms';
-
+import useStyles from './styles';
+import { Typography, Grid, CircularProgress } from '@mui/material';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { getActivities } from '../../actions/activities';
+import Activity from '../../components/organisms/Activities/Activity/Activity';
 // services
 
 import './styles.scss';
 import { useHistory, useLocation } from 'react-router-dom';
 
 const Welcome = () => {
+  const classes = useStyles();
   const history = useHistory();
   const location = useLocation();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
-
+  const dispatch = useDispatch();
   useEffect(() => {
     //const token = user?.token;
-
     setUser(JSON.parse(localStorage.getItem('profile')));
+    dispatch(getActivities());
   }, [location]);
 
+  const activities = useSelector((state) => state.activities);
+  console.log(activities);
   if (user) {
-    console.log('ada');
     console.log(user.result);
     if (user.result.role === 'ketua') {
       if (
@@ -40,10 +46,60 @@ const Welcome = () => {
 
   return (
     <div>
-      <Hero />
-      <Section />
-      <AboutSection />
-      <ContactUs />
+      {user ? (
+        // Greet and time
+        <>
+          {/* // Greet and time */}
+          <div style={{ justifyContent: 'space-between', display: 'flex' }}>
+            <Typography variant="h5" style={{ fontSize: '36px' }}>
+              Selamat datang,{' '}
+              {user.result.jenisKelamin == 'pria' ? 'Pak' : 'Bu'}{' '}
+              {user.result.namaDepan} {user.result.namaBelakang}
+            </Typography>
+            <div className={classes.time}>
+              <CalendarMonthIcon fontSize="20px" style={{ fontSize: '20px' }} />
+              <Typography variant="body1">Selasa, 7 Juni 2022</Typography>
+            </div>
+          </div>
+          {/* // kegiatan */}
+          <div style={{ marginTop: '14px' }}>
+            <Typography
+              variant="h6"
+              style={{ color: '#9D9D9D', marginBottom: '14px' }}
+            >
+              Kegiatan RT 02 Alam Asri 1
+            </Typography>
+
+            <Grid container>
+              <Grid item xs={12} md={12}>
+                {!activities.length ? (
+                  <CircularProgress />
+                ) : (
+                  <Grid
+                    className={classes.container}
+                    container
+                    alignItems="stretch"
+                    spacing={3}
+                  >
+                    {activities.map((activity) => (
+                      <Grid item key={activity.id} xs={12} sm={3}>
+                        <Activity activity={activity} />
+                      </Grid>
+                    ))}
+                  </Grid>
+                )}
+              </Grid>
+            </Grid>
+          </div>
+        </>
+      ) : (
+        <>
+          <Hero />
+          <Section />
+          <AboutSection />
+          <ContactUs />
+        </>
+      )}
     </div>
   );
 };
